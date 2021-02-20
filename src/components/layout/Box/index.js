5dc36c6c4ae004c35210ebe282d62bec2fc5fe76
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-20 23:28:26 +0000

    Create Box

---
+++ src/components/layout/Box/index.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import propToStyle from '../../../theme/utils/propToStyle';
+
+const Box = styled.div`
+  ${propToStyle('display')}
+  ${propToStyle('flexDirection')}
+  ${propToStyle('justifyContent')}
+  ${propToStyle('flex')}
+  ${propToStyle('flexWrap')}
+  ${propToStyle('backgroundImage')}
+  ${propToStyle('backgroundRepeat')}
+  ${propToStyle('backgroundPosition')}
+  background-color: ${({ theme }) => theme.colors.background.main.color};
+`;
+
+export default Box;
