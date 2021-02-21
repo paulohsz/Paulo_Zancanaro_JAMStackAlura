84effb6c8bb176a837e46f2af61be44aac9f18f0
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-21 23:45:52 +0000

    New features (Box)

--- src/components/layout/Box/index.js
+++ src/components/layout/Box/index.js
@@ -1,16 +1,24 @@
 import styled from 'styled-components';
+import polygonClip from '../../../theme/utils/polygonClip';
 import propToStyle from '../../../theme/utils/propToStyle';
 
 const Box = styled.div`
   ${propToStyle('display')}
   ${propToStyle('flexDirection')}
   ${propToStyle('justifyContent')}
+  ${propToStyle('textAlign')}
   ${propToStyle('flex')}
   ${propToStyle('flexWrap')}
+  ${propToStyle('borderTop')}
+  ${propToStyle('backgroundColor')}
   ${propToStyle('backgroundImage')}
   ${propToStyle('backgroundRepeat')}
   ${propToStyle('backgroundPosition')}
-  background-color: ${({ theme }) => theme.colors.background.main.color};
+  
+  ${polygonClip('polygonBottom')}
+  ${polygonClip('polygonTop')}
+
+  background-color: ${({ theme }) => theme.colors.background.main.color};  
 `;
 
 export default Box;
