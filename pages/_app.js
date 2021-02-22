66dfd36dea23e414c844d21627d3298d696eee7e
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-02-22 02:24:32 +0000

    Add shortcut icon

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Paulo H S Zancanaro - Developer - Challenge - JAMStack Front-End (Bootcamp)</title>
+        <link rel="shortcut icon" href="images/icon-cv.svg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap" rel="stylesheet" />
       </Head>
