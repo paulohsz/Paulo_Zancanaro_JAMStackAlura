aa22bc952eb20b8f239341bfda6a29ed847338fd
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-02 22:28:47 +0000

    feat(create a modal): install framer-motion and create a modal component

---
+++ src/components/commons/Modal/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
+import { motion } from 'framer-motion';
+import ModalWrapper from './styles';
+
+const LockScroll = createGlobalStyle`
+  body {
+    overflow: hidden;
+  }
+`;
+
+function Modal({ isOpen, onClose, children }) {
+  return (
+    <ModalWrapper
+      isOpen={isOpen}
+      onClick={(event) => {
+        const isSafeArea = event.target.closest('[data-modal-safe-area="true"]');
+        // isOpen = false;
+        if (!isSafeArea) {
+          onClose();
+        }
+      }}
+    >
+      {isOpen && <LockScroll />}
+      <motion.div
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: '100%',
+          },
+        }}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{
+          display: 'flex',
+          flex: 1,
+        }}
+      >
+        {children({
+          'data-modal-safe-area': 'true',
+        })}
+      </motion.div>
+    </ModalWrapper>
+  );
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  children: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+export default Modal;
