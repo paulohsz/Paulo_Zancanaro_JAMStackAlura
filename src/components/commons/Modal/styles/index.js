5fdbb1a35c2b12dba5663f20fa1a1ebdae89bcac
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-04 22:36:18 +0000

    style(modal): update css attributes

--- src/components/commons/Modal/styles/index.js
+++ src/components/commons/Modal/styles/index.js
@@ -1,30 +1,32 @@
 import styled, { css } from 'styled-components';
 
 const ModalWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  background: rgba(0,0,0,0.75);
+  background: rgba(0,0,0,0.5);
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
-  margin: auto;
-  overflow: scroll;
   transition: .3s;
   z-index: 100;
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  outline: 0;
 
   ${({ isOpen }) => {
     if (isOpen) {
       return css`
         opacity: 1;
         pointer-events: all;
+        display: flex;
+        justify-content: center;
       `;
     }
     return css`
       opacity: 0;
       pointer-events: none;
+      display: none;
     `;
   }}
 `;
