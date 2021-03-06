e776ca6b62d48f7295da0a47144a4f30d135155e
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-06 20:19:09 +0000

    feat(home): include modal in home

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTheme } from 'styled-components';
 import Footer from '@components/commons/Footer';
 import HomeAbout from '@components/commons/HomeAbout';
 import HomeProjects from '@components/commons/HomeProjects';
 import Menu from '@components/commons/Menu';
 import Box from '@components/foundation/layout/Box';
+import Modal from '@components/commons/Modal';
+import FormContact from '@components/patterns/FormContact';
 
 export default function Home() {
   const { background } = useTheme().colors;
+  const [isModalOpen, setModalState] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalState(!isModalOpen);
+  };
 
   return (
     <>
@@ -21,11 +28,23 @@ export default function Home() {
         backgroundColor={background.dark}
         polygonBottom={{ lg: 75, xs: 50 }}
       >
-        <Menu />
+        <Menu contact={handleOpenModal} />
         <HomeAbout />
       </Box>
       <HomeProjects />
-      <Footer />
+      <Footer contact={handleOpenModal} />
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setModalState(false);
+        }}
+      >
+        {(propsDoModal) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <FormContact propsDoModal={propsDoModal} />
+        )}
+      </Modal>
     </>
   );
 }
