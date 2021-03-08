6b6d5e3a72004f11121d7f1354fa87f945621fb2
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-08 21:05:03 +0000

    style(homeabout): animation when load

--- src/components/commons/HomeAbout/index.js
+++ src/components/commons/HomeAbout/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Text from '@components/foundation/Text';
 import Grid from '@components/foundation/layout/Grid';
+import { motion } from 'framer-motion';
 
 export default function HomeAbout() {
   return (
@@ -19,36 +20,69 @@ export default function HomeAbout() {
           justifyContent="center"
           flexDirection="column"
         >
-          <Text
-            variant="title"
-            tag="h1"
-            color="primary.main"
-            textAlign={{
-              xs: 'center',
-              md: 'left',
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+              visible: {
+                x: 0,
+                opacity: 1,
+              },
+              hidden: {
+                x: '-100%',
+                opacity: 0,
+              },
+            }}
+            transition={{
+              duration: 0.5,
             }}
           >
-            Hey, my name is Paulo
-          </Text>
-          <Text
-            variant="paragraph1"
-            tag="p"
-            color="tertiary.main"
-            marginBottom={60}
-            textAlign="justify"
-          >
-            I’m a cleaner and currently live in Ireland.
-            I’m graduated with a BSc in Computer Science, a Postgraduate in Banking IT,
-            and over 10 years of experience in software, system and web development in
-            well-known companies such as ITAU Banking. Currently studying REACT/Next.js
-            (front-end) and eager to put in practice all academic knowledge acquired.
-          </Text>
+            <Text
+              variant="title"
+              tag="h1"
+              color="primary.main"
+              textAlign={{
+                xs: 'center',
+                md: 'left',
+              }}
+            >
+              Hey, my name is Paulo
+            </Text>
+            <Text
+              variant="paragraph1"
+              tag="p"
+              color="tertiary.main"
+              marginBottom={60}
+              textAlign="justify"
+            >
+              I’m a cleaner and currently live in Ireland.
+              I’m graduated with a BSc in Computer Science, a Postgraduate in Banking IT,
+              and over 10 years of experience in software, system and web development in
+              well-known companies such as ITAU Banking. Currently studying REACT/Next.js
+              (front-end) and eager to put in practice all academic knowledge acquired.
+            </Text>
+          </motion.div>
         </Grid.Col>
         <Grid.Col
           offset={{ xs: 0, md: 0 }}
           value={{ xs: 12, md: 6 }}
         >
-          <img
+          <motion.img
+            initial="hidden"
+            animate="visible"
+            variants={{
+              visible: {
+                opacity: 1,
+                scale: 1,
+              },
+              hidden: {
+                opacity: 0,
+                scale: 2,
+              },
+            }}
+            transition={{
+              duration: 0.5,
+            }}
             alt="Paulo H S Zancanaro's profile"
             style={{
               display: 'block',
@@ -60,6 +94,7 @@ export default function HomeAbout() {
             width="235"
             height="308"
           />
+
         </Grid.Col>
       </Grid.Row>
     </Grid.Container>
