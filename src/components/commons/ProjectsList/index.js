53e3d3421fd287bab761b07dc5082731d15d475e
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-23 22:08:38 +0000

    fix: change component name ProjectsGit -> ProjectsList

--- src/components/commons/ProjectsList/index.js
+++ src/components/commons/ProjectsList/index.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useState } from 'react';
-// import PropTypes from 'prop-types';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import Text from '@components/foundation/Text';
 import Box from '@components/foundation/layout/Box';
 import Grid from '@components/foundation/layout/Grid';
 import LinksProjects from './styles/Projects';
 
-export default function ProjectsGit() {
-  const [gitProjects, setGitProjects] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.github.com/users/paulohsz/repos').then(async (res) => {
-      const response = await res.json();
-      return response;
-    })
-      .then((data) => {
-        setGitProjects(data);
-      });
-  }, []);
-
+export default function ProjectsList({ projects, title }) {
   return (
     <Grid.Container flexGrow={1}>
       <Grid.Row>
         <Grid.Col
           offset={{ xs: 0, sm: 1 }}
+          s
           value={{ xs: 12, md: 10 }}
           display="flex"
           alignItems="flex-start"
@@ -36,12 +24,12 @@ export default function ProjectsGit() {
             color="secundary.main"
             textAlign="left"
           >
-            Projects
+            {title}
           </Text>
           <Box paddingBottom={24} display="flex" flexDirection="column" flexGrow={1}>
-            {gitProjects.map((project) => (
-              <LinksProjects key={project.name} href={`/projects/${project.name}`}>
-                {(project.description || project.name)}
+            {projects.map((project) => (
+              <LinksProjects key={project.href} href={project.href} target={(project.target || '_self')}>
+                {project.title}
               </LinksProjects>
             ))}
           </Box>
@@ -51,10 +39,12 @@ export default function ProjectsGit() {
   );
 }
 
-ProjectsGit.defaultProps = {
-  // white: false, // About background (dark: blue, white: white)
+ProjectsList.defaultProps = {
+  projects: [],
+  title: 'Projects',
 };
 
-ProjectsGit.propTypes = {
-  // white: PropTypes.bool,
+ProjectsList.propTypes = {
+  projects: PropTypes.instanceOf(Array),
+  title: PropTypes.string,
 };
